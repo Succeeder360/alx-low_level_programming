9d95e946dfdfeb10a97c60b48e0e72ee5fcc408c
Succeeder360 <succeeder419@gmail.com>
2023-03-16 06:20:48 -0700

    a long long int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 char a_char;
 int an_int;
 long int a_long_int;
-long long a_long_long_int;
+long long int  a_long_long_int;
 float a_float;
 
 printf("size of int: %i byte(s)\n", sizeof(a_char));
-printf(size of float: %i byte(s)\n", sizeof(an_int));
+printf("size of float: %i byte(s)\n", sizeof(an_int));
 printf("size of char: %i byte(s)\n", sizeof(a_long_int));
 printf("size of double:%i byte(s)\n", sizeof(a_long_long_int));
 printf("size of long long int: %i byte(s)\n", sizeof(a_float);
