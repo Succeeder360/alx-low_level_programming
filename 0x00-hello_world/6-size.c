8d4b53d58c2b5ecc370eab9b7d69742a75c75341
Succeeder360 <succeeder419@gmail.com>
2023-03-16 07:22:05 -0700

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@ int i;
 long int il;
 long long int ill;
 float a;
-printf("size of a char: %lu byte(s)\n", sizeof(ab));
-printf("size of an int: %lu byte(s)\n", sizeof(i));
-printf("size of a long int: %lu byte(s)\n", sizeof(il));
-printf("size of a long long int: %lu byte(s)\n", sizeof(ill));
-printf("size of a float: %lu byte(s)\n", sizeof(a));
+printf("Size of a char: %lu byte(s)\n", sizeof(ab));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(il));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(ill));
+printf("Size of a float: %lu byte(s)\n", sizeof(a));
 
 return (0);
 }
