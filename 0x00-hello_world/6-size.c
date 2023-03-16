8e2c6e1959e864900927b06514b19a048e02904c
Succeeder360 <succeeder419@gmail.com>
2023-03-16 05:21:06 -0700

    added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("size of int %d\n", sizeof(char));
-printf("size of float %d\n", sizeof(int));
-printf("size of char %d\n", sizeof(long int));
-printf("size of double %d\n", sizeof(long long int));
-printf("size of long long int %d\n", sizeof(float);
+printf("size of int: %i byte(s)\n", sizeof(char));
+printf("size of float: %i byte(s)\n", sizeof(int));
+printf("size of char: %i byte(s)\n", sizeof(long int));
+printf("size of double:%i byte(s)\n", sizeof(long long int));
+printf("size of long long int: %i byte(s)\n", sizeof(float);
 return (0);
 }
