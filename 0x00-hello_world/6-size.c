98aa3a07a68300597b62acadd41a6558259e2251
Succeeder360 <succeeder419@gmail.com>
2023-03-16 05:01:25 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio>
+/**
+ * main- program execution starts here
+ * sizeof(): returns sizes of various types
+ * Return: returns 0
+ */
+int main(void)
+{
+printf("size of int %d\n", sizeof(int));
+printf("size of float %d\n", sizeof(float));
+printf("size of char %d\n", sizeof(char));
+printf("size of double %d\n", sizeof(double));
+return (0);
+}
