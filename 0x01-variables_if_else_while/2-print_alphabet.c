a16965b728827960e8c7cdfda1be69c596f6e471
Succeeder360 <succeeder419@gmail.com>
2023-03-17 07:36:29 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entery point
+ * Return: returns 0
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+putchar('\n');
+alphabet++;
+}
+
+return (0);
+}
