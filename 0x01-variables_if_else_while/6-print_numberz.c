913d6fd694b126e04f5e781022223241832a0ed9
Succeeder360 <succeeder419@gmail.com>
2023-03-17 09:48:11 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entery point
+ * Return: returns 0
+ */
+int main(void)
+{
+int base10;
+for (base10 = 0; base10 < 10; base10++)
+{
+putchar(base10 + '0');
+}
+putchar('\n');
+
+return (0);
+}
