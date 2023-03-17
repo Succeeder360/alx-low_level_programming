fc858a1713a2acb38ad7ab59c22a1d9f52db4160
Succeeder360 <succeeder419@gmail.com>
2023-03-17 08:38:39 -0700

    lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+char alp;
+for(alp = 'a'; alp <='z'; alp++)
+{
+putchar(alp);
+}
+for (alp = 'A'; alp <= 'Z'; alp++)
+{
+putchar(alp);
+}
+putchar('\n');
+return 0;
+}
