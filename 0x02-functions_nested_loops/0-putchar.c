27986edf3784cbd84d7f02f25a7a1af25d499cec
Succeeder360 <succeeder419@gmail.com>
2023-03-21 06:33:13 -0700

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,14 @@
  */
 int main(void)
 {
-char str[8] = "_putchar";
-for (int i = 0; i <= 7; i++)
-{
-_putchar(str[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
