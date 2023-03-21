5e28031889d02bd9a30973c8d186cfc770c9119d
Succeeder360 <succeeder419@gmail.com>
2023-03-21 07:01:34 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main- Main entry
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+char a_z;
+for (a_z = 'a'; a_z <= 'z'; a_z++);
+_putchar(a_z);
+_putchar('\n');
+}
+
