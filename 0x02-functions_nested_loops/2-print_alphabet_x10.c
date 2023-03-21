ef93e0f17bab0a5166cca034086f3a0226c8d418
Succeeder360 <succeeder419@gmail.com>
2023-03-21 07:40:34 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10- print alphabet 10 times
+ * Return: return 0 
+ */
+void print_alphabet_x10(void)
+{
+char a_z;
+int i;
+i = 0;
+while (i < 10)
+{
+ch = 'a';
+while (a_z <= 'z')
+{
+_putchar(a_z);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
